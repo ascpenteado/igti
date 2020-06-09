7fed5958e3e58be5c05319bec110dcccb991538e
Arthur Penteado <ascpenteado@gmail.com>
2020-06-09 01:06:09 -0300

    Finished tasks 2, 3 and 4

--- desafio02/index.js
+++ desafio02/index.js
@@ -34,11 +34,52 @@ app.post('/', async (req, res) => {
     res.send(gradesData);
 });
 
+app.put('/:id', async (req, res) => {
+    let id = parseInt(req.params.id);
+    let { student, subject, type, value } = req.body;
+    let gradesData = JSON.parse(await readFile('./grades.json', 'utf8'));
+
+    let gradeToUpdate = gradesData.grades.find((grade) => grade.id === id);
+    if (gradeToUpdate === undefined) {
+        res.status(404).json({ error: 'id não encontrado' });
+    }
+    gradeToUpdate.student = student;
+    gradeToUpdate.subject = subject;
+    gradeToUpdate.type = type;
+    gradeToUpdate.value = value;
+
+    await writeFile('./grades.json', JSON.stringify(gradesData), 'utf8');
+    res.send(gradeToUpdate);
+});
+
+app.delete('/:id', async (req, res) => {
+    let id = parseInt(req.params.id);
+    let gradesData = JSON.parse(await readFile('./grades.json', 'utf8'));
+
+    let gradeToDelete = gradesData.grades.findIndex((grade) => grade.id === id);
+    if (gradeToDelete === -1) {
+        res.status(404).json({ error: 'id não encontrado' });
+    }
+
+    gradesData.grades.splice(gradeToDelete, 1);
+
+    await writeFile('./grades.json', JSON.stringify(gradesData), 'utf8');
+    res.send(gradesData);
+});
+
+app.get('/:id', async (req, res) => {
+    let id = parseInt(req.params.id);
+    let gradesData = JSON.parse(await readFile('./grades.json', 'utf8'));
+
+    let gradeToShow = gradesData.grades.find((grade) => grade.id === id);
+    if (gradeToShow === undefined) {
+        res.status(404).json({ error: 'id não encontrado' });
+    }
+    res.send(gradeToShow);
+});
+
 /*  TO DO ***
 
-2. Crie um endpoint para atualizar uma grade. Esse endpoint deverá receber como parâmetros o id da grade a ser alterada e os campos student, subject, type e value. O endpoint deverá validar se a grade informada existe, caso não exista deverá retornar um erro. Caso exista, o endpoint deverá atualizar as informações recebidas por parâmetros no registro, e realizar sua atualização com os novos dados alterados no arquivo grades.json.
-3. Crie um endpoint para excluir uma grade. Esse endpoint deverá receber como parâmetro o id da grade e realizar sua exclusão do arquivo grades.json.
-4. Crie um endpoint para consultar uma grade em específico. Esse endpoint deverá receber como parâmetro o id da grade e retornar suas informações.
 5. Crie um endpoint para consultar a nota total de um aluno em uma disciplina. O endpoint deverá receber como parâmetro o student e o subject, e realizar a soma de todas as notas de atividades correspondentes àquele subject, para aquele student. O endpoint deverá retornar a soma da propriedade value dos registros encontrados.
 6. Crie um endpoint para consultar a média das grades de determinado subject e type. O endpoint deverá receber como parâmetro um subject e um type, e retornar a média. A média é calculada somando o registro value de todos os registros que possuem o subject e type informados, dividindo pelo total de registros que possuem este mesmo subject e type.
 7. Crie um endpoint para retornar as três melhores grades de acordo com determinado subject e type. O endpoint deve receber como parâmetro um subject e um type, e retornar um array com os três registros de maior value daquele subject e type. A ordem deve ser do maior para o menor. 

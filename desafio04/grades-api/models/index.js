a93c1e265bf2809403b019099a2922259536b242
Arthur Penteado <ascpenteado@gmail.com>
2020-07-12 14:19:19 -0300

    added desafio04

---
+++ desafio04/grades-api/models/index.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const db = {};
+db.mongoose = mongoose;
+db.url = process.env.MONGODB;
+
+const gradeSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    subject: { type: String, required: true },
+    type: { type: String, required: true },
+    value: { type: Number, required: true },
+    lastModified: { type: Date, required: true },
+});
+
+const GradeModel = mongoose.model('Grade', gradeSchema);
+
+export { db, GradeModel };
